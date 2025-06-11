79d9f98bd044ad333454e84186316d5a2b0885d0
v-rk1t <air.veerakit@gmail.com>
2025-06-11 21:31:36 +0700

    test: add schema test in api test

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from "@playwright/test";
+import { listUsersRespSchema, UserDetails } from "../../schemas/reqres";
+import { userData } from "../../data/user";
 
 test("Assignment 1", async ({ request }) => {
   const response = await request.get("https://reqres.in/api/users?page=2", {
     headers: { "x-api-key": "reqres-free-v1" },
   });
+
   const responseBody = await response.json();
+  expect(() => {
+    listUsersRespSchema.parse(responseBody);
+  }).not.toThrow();
 
   // Assert response "total_pages" equal to 2
   expect(responseBody.total_pages).toEqual(2);
@@ -14,6 +20,8 @@ test("Assignment 1", async ({ request }) => {
   expect(response.status()).toBe(200);
 
   // Assert if the user with id=10 has an email equal to byron.fields@reqres.in
-  const targetUser = responseBody.data.find((user) => user.id === 10);
-  expect(targetUser.email).toEqual("byron.fields@reqres.in");
+  const targetUser = responseBody.data.find(
+    (user: UserDetails) => user.id === userData.id
+  );
+  expect(targetUser.email).toEqual(userData.email);
 });
